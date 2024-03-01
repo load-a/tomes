8af4cc4c3aec4f7ff88c7b8c6f63762bd65534e4
Load A <143745153+load-a@users.noreply.github.com>
2024-02-29 23:50:43 -0500

    Require QoL

--- deck_of_cards/cards.rb
+++ deck_of_cards/cards.rb
@@ -4,6 +4,7 @@
 # Note that the order of attributes is important when referencing Cards by their values.
 
 class Card
+	# @return [Hash] The hash table for each attribute name and value.
 	attr_reader :card_attributes
 
 	def initialize(attributes, values)
@@ -16,6 +17,7 @@ class Card
 		
 	end
 
+	# @return [Array] An array version of #card_attributes
 	def to_a
 		@card_attributes.to_a
 	end
@@ -31,24 +33,32 @@ class Card
 		output
 	end
 
+	# @param [<String, Symbol>, object]
+
 	def add_attribute(type, value)
 		@card_attributes[type.to_sym] = value
 	end
 
+	# @param [String, Symbol] The attribute name you want to remove.
+	# @return [void] Removes it from the #card_attributes hash. 
+	# @note It is possible to erase the two default attributes this way, but #suit and #value will return nil.
 	def remove_attribute(type)
 		@card_attributes.reject! {|card_type| card_type.to_s == type}
 	end
 
+	# @return [String] The 'suit' attribute value as a String.
 	def suit
 		@card_attributes[:suit]
 	end
 
 	##
-	# Default Value
+	# @return [String] The 'value' attribute value as a String.
+	# @note This is the value parameter assigned at initialization.
 	def value
 		@card_attributes[:value]
 	end
 
+	# @return [Array<Arrays>] Converts each key-value pair into an array of strings, then returns those withing an array.
 	def type_values
 		@card_attributes.each_value.to_a
 	end
