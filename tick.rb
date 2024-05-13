1f6d59b761d5185c6f93ff11c991262d5a84fe88
Load A <143745153+load-a@users.noreply.github.com>
2024-05-13 17:40:42 -0400

    Forgot to clear this file

--- tick.rb
+++ tick.rb
@@ -1,14 +1,3 @@
 def tick(args)
 
-  DevTools::update(args) 
-  Keyboard::update(args)
-  args.state.ob ||= Keyboard
-
-  args.state.ls = [
-    args.state.ob,
-    Keyboard::letter_hold
-  ]
-
-  DevTools.list([100, 700], Keyboard.instance_methods.sort - Object.instance_methods)
-  DevTools.list([500, 700], args.state.ls)
 end
